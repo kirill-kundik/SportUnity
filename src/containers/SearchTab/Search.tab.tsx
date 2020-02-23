9be97713a95438d64b65a3317b3ddbbac7b0f633
Maksym <maxim4110@gmail.com>
2020-02-23 06:28:14 +0200

    submit input done

--- src/containers/SearchTab/Search.tab.tsx
+++ src/containers/SearchTab/Search.tab.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, {useCallback, useState} from 'react'
 import {View} from 'react-native'
+import {Container, SubmitButton, SubmitText, TextInput} from './Search.styles'
 
 export default function SearchTab() {
-	return <View>
-		Search Tab
-	</View>
+	const [enteredText, setEnteredText] = useState('')
+	const onSubmit = (text: string) => {
+
+	}
+	return <Container>
+		<TextInput
+			onChangeText={useCallback((text) => {
+				setEnteredText(text)
+			}, [setEnteredText])}
+		/>
+		<SubmitButton
+			onPress={useCallback(() => {
+				onSubmit(enteredText)
+			}, [enteredText])}
+		>
+			<SubmitText
+			>
+				Submit
+			</SubmitText>
+		</SubmitButton>
+	</Container>
 }
